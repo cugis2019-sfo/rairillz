f12747b32b1b07aacb05c86bad7f6345efd70567
raicode2021 <51681150+raicode2021@users.noreply.github.com>
2019-06-10 16:52:26 -0700

    Add files via upload

---
+++ coding class1a.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 14:09:18 2019
+
+@author: admin
+"""
+5/8 #division
+print("hello",5,"world")
+print("I am learning to code in python today")
+print("pray down christianna",5,"I wanna see you pray down")
+print ("5")
+5*2 #multiplication
+5/2 #division
+5-2 #subraction
+5**2 #exponents
+5*2 #multiplication
+def sum(a,b):
+    sum = a + b
+    print("the sum of",a, "and", b"is", sum)
+
+sum(4,5)
+sum(3.6, 4.9)
+
+def trianglearea (b,h,0.5):
+    sum= b * h *0.5
+    print("the area of the triangle is")
+    sum(b,h,0.5)fpy
